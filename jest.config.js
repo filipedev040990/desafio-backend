87241339d25a383a7928159fb69d63c0d292498b
Filipe Siqueira <filipedev040990@gmail.com>
2023-11-20 20:57:24 -0300

    feat: implements update user and getbyid in user repository

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   collectCoverageFrom: [
-    '<rootDir>/src/**/*.ts'
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*.type.ts',
+    '!<rootDir>/src/**/*.interface.ts',
+    '!<rootDir>/src/**/*.helper.ts',
+    '!<rootDir>/src/**/*.factory.ts',
+    '!<rootDir>/src/**/*.app.ts',
+    '!<rootDir>/src/**/*.server.ts'
   ],
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
--- src/infra/database/repositories/user.repository.ts
+++ src/infra/database/repositories/user.repository.ts
@@ -1,5 +1,5 @@
 
-import { UserRepositoryInterface, UserOutput, CreateUserRepositoryInput } from '@/application/interfaces/repositories'
+import { UserRepositoryInterface, UserOutput, CreateUserRepositoryInput, UpdateUserRepositoryInput } from '@/application/interfaces/repositories'
 import { prismaClient } from '../config/prisma-client'
 
 export class UserRepository implements UserRepositoryInterface {
@@ -26,6 +26,7 @@ export class UserRepository implements UserRepositoryInterface {
         id: input.id,
         name: input.name,
         email: input.email,
+        active: input.active,
         password: input.password,
         permissions: input.permissions,
         createdAt: input.createdAt
@@ -33,5 +34,30 @@ export class UserRepository implements UserRepositoryInterface {
     })
 
     return user.id
+  }
+
+  async getById (id: string): Promise<UserOutput | null> {
+    const user = await prismaClient.user.findFirst({ where: { id } })
+    if (!user) {
+      return null
+    }
+
+    const permissions: number [] = []
+    user.permissions.split(',').map((permission: string) => permissions.push(+permission))
+
+    return {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      permissions
+    }
+  }
+
+  async update (input: UpdateUserRepositoryInput): Promise<void> {
+    const { id, updatedAt, ...inputWithoutId } = input
+    await prismaClient.user.update({
+      where: { id },
+      data: { ...inputWithoutId, updatedAt }
+    })
   }
 }
