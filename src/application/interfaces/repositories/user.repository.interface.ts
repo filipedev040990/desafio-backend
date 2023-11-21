a316e9ac2739cb36676dd66d1941b4ec2737bedc
Filipe Siqueira <filipedev040990@gmail.com>
2023-11-21 20:56:01 -0300

    refactor: change types

--- src/application/interfaces/repositories/user.repository.interface.ts
+++ src/application/interfaces/repositories/user.repository.interface.ts
@@ -3,7 +3,18 @@ export type UserOutput = {
   name: string
   email: string
   permissions: number []
-}
+} | null
+
+export type UserAllInfoOutput = {
+  id: string
+  name: string
+  email: string
+  password: string
+  active: number
+  permissions: number []
+  createdAt: Date
+  updatedAt?: Date
+} | null
 
 export type CreateUserRepositoryInput = {
   id: string
@@ -26,8 +37,9 @@ export type UpdateUserRepositoryInput = {
 }
 
 export interface UserRepositoryInterface {
-  getByEmail: (email: string) => Promise<UserOutput | null>
+  getByEmail: (email: string) => Promise<UserOutput>
+  getAllInfo: (email: string) => Promise<UserAllInfoOutput>
   create: (input: CreateUserRepositoryInput) => Promise<string>
-  getById: (id: string) => Promise<UserOutput | null>
+  getById: (id: string) => Promise<UserOutput>
   update: (input: UpdateUserRepositoryInput) => Promise<void>
 }
