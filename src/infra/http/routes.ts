7c20ed722ed927edc72b8fe8c37327651438c7d4
Filipe Siqueira <filipedev040990@gmail.com>
2023-11-23 09:22:39 -0300

    feat: add authenticate route

--- src/infra/http/routes.ts
+++ src/infra/http/routes.ts
@@ -2,10 +2,13 @@ import { expressAdapter } from '@/adapters/tools/express/express.adapter'
 import { Router } from 'express'
 import { createUserControllerFactory } from '../factories/controllers/create-user.factory'
 import { updateUserControllerFactory } from '../factories/controllers/update-user.factory'
+import { authenticateControllerFactory } from '../factories/controllers/authenticate.factory'
 
 const router = Router()
 
 router.post('/user', expressAdapter(createUserControllerFactory()))
 router.patch('/user/:id', expressAdapter(updateUserControllerFactory()))
+
+router.post('/authenticate', expressAdapter(authenticateControllerFactory()))
 
 export { router }
