dae945a06f05293b8fa3cd240fc8b565cd1b07f6
Filipe Siqueira <filipedev040990@gmail.com>
2023-11-23 09:16:49 -0300

    feat: implements authenticate controller and tests

---
+++ src/adapters/controllers/authentication/authenticate.controller.spec.ts
@@ -0,0 +1,49 @@
+import { AuthenticateUseCaseInterface } from '@/application/usecases/authentication/authenticate.types'
+import { HttpRequest } from '@/shared/types'
+import { AuthenticateController } from './authenticate.controller'
+import { mock } from 'jest-mock-extended'
+import { serverError, success } from '@/shared/helpers'
+
+describe('AuthenticateController', () => {
+  let sut: AuthenticateController
+  let input: HttpRequest
+
+  const authenticateUseCase = mock<AuthenticateUseCaseInterface>()
+
+  beforeEach(() => {
+    sut = new AuthenticateController(authenticateUseCase)
+    input = {
+      body: {
+        email: 'anyEmail@email.com',
+        password: 'anyPassword'
+      }
+    }
+
+    authenticateUseCase.execute.mockResolvedValue({ token: 'anyToken' })
+  })
+
+  test('should call AuthenticateUseCase once and with correct values', async () => {
+    await sut.execute(input)
+
+    expect(authenticateUseCase.execute).toHaveBeenCalledTimes(1)
+    expect(authenticateUseCase.execute).toHaveBeenCalledWith(input.body)
+  })
+
+  test('should return 200 and a token on success', async () => {
+    const output = await sut.execute(input)
+
+    expect(output).toEqual(success(200, { token: 'anyToken' }))
+  })
+
+  test('should return 500 if AuthenticateUseCase throws an exception', async () => {
+    const error = new Error('Any Error')
+
+    authenticateUseCase.execute.mockImplementationOnce(() => {
+      throw error
+    })
+
+    const output = await sut.execute(input)
+
+    expect(output).toEqual(serverError(error))
+  })
+})
---
+++ src/adapters/controllers/authentication/authenticate.controller.ts
@@ -0,0 +1,17 @@
+import { ControllerInterface } from '@/application/interfaces/controllers/general.controller.interface'
+import { AuthenticateUseCaseInterface } from '@/application/usecases/authentication/authenticate.types'
+import { success } from '@/shared/helpers'
+import { handleError } from '@/shared/helpers/handle-error.helper'
+import { HttpRequest, HttpResponse } from '@/shared/types'
+
+export class AuthenticateController implements ControllerInterface {
+  constructor (private readonly authenticateUseCase: AuthenticateUseCaseInterface) {}
+  async execute (input: HttpRequest): Promise<HttpResponse> {
+    try {
+      const token = await this.authenticateUseCase.execute(input?.body)
+      return success(200, token)
+    } catch (error: any) {
+      return handleError(error)
+    }
+  }
+}
