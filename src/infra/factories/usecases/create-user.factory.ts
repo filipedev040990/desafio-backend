113d0913159af95e1fe85ce49e271468421c898a
Filipe Siqueira <filipedev040990@gmail.com>
2023-11-23 09:24:23 -0300

    refactor: remove unused dependencies of CreateUseCaseFactory

--- src/infra/factories/usecases/create-user.factory.ts
+++ src/infra/factories/usecases/create-user.factory.ts
@@ -1,5 +1,4 @@
 import { BcryptAdapter } from '@/adapters/tools/crypto/bcrypt.adapter'
-import { JwtAdapter } from '@/adapters/tools/token/jwt.adapter'
 import { UUIDGenerator } from '@/adapters/tools/uuid/uuid.adapter'
 import { CreateUserUseCase } from '@/application/usecases/users/create/create-user.usecase'
 import { UserRepository } from '@/infra/database/repositories/user.repository'
@@ -8,8 +7,5 @@ export const createUserUseCaseFactory = (): CreateUserUseCase => {
   const userRepository = new UserRepository()
   const uuidGenerator = new UUIDGenerator()
   const hashGenerator = new BcryptAdapter(12)
-  const secretKey = process.env.SECRETKEY ?? ''
-  const expiresIn = process.env.EXPIRESIN ?? ''
-  const tokenGenerator = new JwtAdapter(secretKey, expiresIn)
-  return new CreateUserUseCase(userRepository, uuidGenerator, hashGenerator, tokenGenerator)
+  return new CreateUserUseCase(userRepository, uuidGenerator, hashGenerator)
 }
